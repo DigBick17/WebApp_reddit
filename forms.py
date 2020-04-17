04e55325a498eca7b8220de135e022905324f664
Soham Bhardwaj <53705102+DigBick17@users.noreply.github.com>
2020-04-18 01:27:17 +0530

    Delete forms.py

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length
-
-class userinput(FlaskForm):
-    username1=StringField('First Username', validators=[DataRequired(), Length(min=2, max=20)])
-    username2=StringField('Second Username', validators=[DataRequired(), Length(min=2, max=20)])
-    submit=SubmitField('Compare')
