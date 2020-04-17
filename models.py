fe14cd139df6740db4e3fd576f9fd6633f941bfa
Soham Bhardwaj <53705102+DigBick17@users.noreply.github.com>
2020-04-18 01:27:25 +0530

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from main import db
-
-class User(db.Model):
-	id=db.Column(db.Integer, primary_key=True)
-	Username1=db.Column(db.String(20))
-	Username2=db.Column(db.String(20))
-
-	def __repr__(self):
-		return f"User('{self.Username1}','{self.Username2}')"
